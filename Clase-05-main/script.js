e1cd2f7ff2e06604d8797769fdb107568e985926
Isaac911GH <giorgio.wojciechowski@inacapmail.cl>
2025-05-26 15:46:22 -0400

    Estadistica

--- Clase-05-main/script.js
+++ Clase-05-main/script.js
@@ -35,6 +35,7 @@ form.addEventListener("submit", function(e) {
 
     this.reset();
     calcularPromedio();
+    actualizarestadisticas()
 });
 
 function addStudentToTable(student) {
@@ -73,6 +74,7 @@ function deleteEstudiante(student, row) {
         students.splice(index, 1);
         row.remove();
         calcularPromedio();
+        actualizarestadisticas()
     }
 }
 
@@ -98,9 +100,24 @@ function calcularPromedio() {
     }
 }
 
+function actualizarestadisticas(){
+    const total = students.length;
+    const aprobados = students.filter(est => est.grade >= 4.0).length;
+    const reprobados =total - aprobados;
+
+    const statsDiv=document.getElementById("estadisticas");
+    statsDiv.innerHTML = `
+        <p> Total de estudiantes ${total} </p>
+        <p> Cantidad de aprobados ${aprobados} </p>
+        <p> Cantidad de reprobados ${reprobados}</p>
+    `;
+}
+
 function updateTable() {
     tableBody.innerHTML = "";
     students.forEach(student => {
         addStudentToTable(student);
     });
+    calcularPromedio();
+    actualizarestadisticas();
 }
